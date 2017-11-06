0679178b236d9d95d31c53517032f56552df419d
jeffrey1hu <hu1chao@126.com>
2017-11-06 16:16:37 +0800

    debug

--- code/test/read_data_test.py
+++ code/test/read_data_test.py
@@ -8,7 +8,7 @@ def test_mask_dataset():
     dataset = mask_dataset(data_dir, set_names=['train', 'val'], suffixes=['context', 'question'])
     print dataset.keys()
     for k, v in dataset.iteritems():
-        print k, v[:3]
+        print k, len(v), v[:3]
 
 
 if __name__ == '__main__':
