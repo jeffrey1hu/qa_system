ff0e056f97725a68a7bf06e42e1087bfe1c6f797
jeffrey1hu <hu1chao@126.com>
2017-11-06 16:34:15 +0800

    debug

--- code/test/read_data_test.py
+++ code/test/read_data_test.py
@@ -2,6 +2,8 @@
 import os
 import sys
 import logging
+logging.basicConfig(level=logging.INFO)
+
 path = '/'.join(os.path.abspath(__file__).split('/')[:-2])
 sys.path += [path + x for x in ['']]
 
@@ -9,17 +11,21 @@ sys.path += [path + x for x in ['']]
 from utils.read_data import mask_dataset, read_raw_answers, read_answers
 
 data_dir = path + "/data/squad"
-print data_dir
-
 
 def test_mask_dataset():
+    logging.info("test_mask_dataset")
     dataset = mask_dataset(data_dir, set_names=['train', 'val'], suffixes=['context', 'question'])
     print dataset.keys()
     for k, v in dataset.iteritems():
-        print k, len(v), v[:3]
+        print k, len(v), v[0]
 
 def test_read_raw_answers():
+    logging.info("test_read_raw_answers")
     raw_answers = read_answers(data_dir)
+    print read_answers.keys()
+    for k, v in raw_answers.iteritems():
+        print k, len(v), v[0]
 
 if __name__ == '__main__':
     test_mask_dataset()
+    test_read_raw_answers()
