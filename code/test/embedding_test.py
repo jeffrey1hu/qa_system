f6507f4aaf3e5b5451ab8387b58f36ba928de3a5
jeffrey1hu <hu1chao@126.com>
2017-11-06 16:57:32 +0800

    debug

--- code/test/embedding_test.py
+++ code/test/embedding_test.py
@@ -20,7 +20,7 @@ data_dir = cfg.DATA_DIR
 def embed_test(print_out = 10):
     vocab_path = pjoin(data_dir, 'vocab.dat')
     vocab, rev_vocab = initialize_vocab(vocab_path)
-    print('first {} of vocab: {}'.format(print_out, ''.join(vocab[:print_out].items())))
+    print('first {} of vocab: {}'.format(print_out, str(vocab.items()[:print_out])))
     print('first {} of rev_vocab: {}'.format(print_out, rev_vocab[:print_out]))
     # we use glove with 100-d in default.
     embed_path = pjoin(data_dir, "glove.trimmed.100.npz")
