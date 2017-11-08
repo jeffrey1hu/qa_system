9961d0ddc308ab486aeba9416ec1cfa48dd1d7ac
jeffrey1hu <hu1chao@126.com>
2017-11-08 12:09:29 +0800

    debug

--- code/test/model_test.py
+++ code/test/model_test.py
@@ -104,6 +104,73 @@ def test_model(num_examples, context=None, question=None, embedding=None, answer
         assert (num_examples, context_max_len, 2 * num_hidden) == H_context.shape, \
             'the shape of H_r should be {} but it is {}'.format((num_examples, context_max_len, 2 * num_hidden),
                                                                                           H_context.shape)
+        # Decoder session
+        H_r_shape = H_r.get_shape().as_list()
+        initializer = tf.contrib.layers.xavier_initializer()
+
+        with tf.variable_scope("decoder"):
+            W_r = tf.get_variable("V_r", shape=[num_hidden * 4, num_hidden * 2], dtype=tf.float32, initializer=initializer)
+
+            W_f = tf.get_variable("W_f", shape=[num_hidden * 2, 1], dtype=tf.float32, initializer=initializer)
+
+            W_h = tf.get_variable("W_h", shape=[num_hidden * 4, num_hidden * 2], dtype=tf.float32, initializer=initializer)
+
+            B_r = tf.get_variable("B_r", shape=[num_hidden * 2], dtype=tf.float32)
+            B_f = tf.get_variable("B_f", shape=[], dtype=tf.float32)
+
+            W_r_e = tf.tile(tf.expand_dims(W_r, axis=0), multiples=[H_r_shape[0], 1, 1])
+            W_f_e = tf.tile(tf.expand_dims(W_f, axis=0), multiples=[H_r_shape[0], 1, 1])
+            W_h_e = tf.tile(tf.expand_dims(W_h, axis=0), multiples=[H_r_shape[0], 1, 1])
+
+            # f1 -> (b, q, 2n)
+            f1 = tf.nn.tanh(tf.matmul(H_r, W_r_e) + B_r)
+            # s_score -> (b, q, 1)
+            s_score = tf.matmul(f1, W_f_e) + B_f
+            # s_score -> (b, q)
+            s_score = tf.squeeze(s_score, axis=2)
+            logging.info('shape of s_score is {}'.format(s_score.shape))
+
+            # the prob distribution of start index
+            s_prob = tf.nn.softmax(s_score)
+            s_prob = s_prob * context_m
+            # Hr_attend -> (batch_size, 4n)
+            Hr_attend = tf.reduce_sum(H_r * tf.expand_dims(s_prob, axis=2), axis=1)
+
+            f2 = tf.nn.tanh(tf.matmul(H_r, W_r_e)
+                            + tf.matmul(tf.tile(tf.expand_dims(Hr_attend, axis=1), multiples=[1, H_r_shape[1], 1]), W_h_e)
+                            + B_r)
+
+            e_score = tf.matmul(f2, W_f_e) + B_f
+            e_score = tf.squeeze(e_score, axis=2)
+            logging.info('shape of e_score is {}'.format(e_score.shape))
+
+            # e_prob = tf.nn.softmax(e_score)
+            # e_prob = tf.multiply(e_prob, context_m)
+
+            loss_s = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=answer_s, logits=s_score)
+            loss_e = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=answer_e, logits=e_score)
+
+            final_loss = tf.reduce_mean(loss_e + loss_s)
+
+            train_op = tf.train.AdadeltaOptimizer().minimize(final_loss)
+            outputs = [H_context, H_question, H_r, s_score, e_score, prob_s, answer_s, answer_e,loss_s,loss_e, final_loss]
+            sess.run(tf.global_variables_initializer())
+            out = sess.run(outputs, feed_dict={context:context_data, context_m:context_masks,
+                                                                  question: question_data, question_m:question_masks,
+                                                                  answer_s:answer_start, answer_e:answer_end,
+                                                                  num_example:num_examples})
+            print('test success.')
+            logging.info('loss is : {}'.format(out[-1]))
+            logging.info('H_context: {}'.format(out[0]))
+            logging.info('H_question: {}'.format(out[1]))
+            logging.info('H_r : {}'.format(out[2]))
+            logging.info('s_score: {}'.format(out[3]))
+            logging.info('e_score: {}'.format(out[4]))
+            logging.info('prob_s : {}'.format(out[5]))
+            logging.info('answer prob_s : {}'.format(out[5][0][out[6]]))
+            logging.info('answers : {}, {}'.format(out[6], out[7]))
+            logging.info('loss: {}, {}'.format(out[8], out[9]))
+
     pass
 
 
